ffb2625060bff3dca734f742231051fa944dafbf
sunqiongzai <sunqiongzai@rayshape.com>
2024-10-10 09:35:30 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# cpp-class-creator 
+
+Create cpp class files.
+
+## Features
+
+### Create c++ class
+
+Right click on the vscode explorer folder
+
+
---
+++ src/helper.ts
@@ -0,0 +1,32 @@
+import * as vscode from 'vscode'
+import path from 'path'
+export async function showCreateNameInputBox(): Promise<string | undefined> {
+  return vscode.window.showInputBox({
+    // placeHolder: 'Please enter the name of the new file',
+    prompt: 'Enter the name of the new class',
+    value: 'newClass'
+  })
+}
+
+export async function createClass(name: string, root: string) {
+  console.log(name, root)
+
+  const head = `#pragma once
+class ${name} {
+};
+
+`
+  const t1 = vscode.workspace.fs.writeFile(
+    vscode.Uri.file(path.join(root, `${name}.h`)),
+    Buffer.from(head)
+  )
+
+  const content = `#include "${name}.h" 
+
+`
+
+  const t2 = vscode.workspace.fs.writeFile(
+    vscode.Uri.file(path.join(root, `${name}.cpp`)),
+    Buffer.from(content)
+  )
+}
